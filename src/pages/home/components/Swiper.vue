f39ee8c52834a04a6e47061c552208386e8dce75
mojingman <1393222082@qq.com>
2019-06-18 17:42:20 +0800

    add icons

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -15,24 +15,24 @@
 </template>
 
 <script>
-    export default {
-      name: "HomeSwiper",
-      data(){
-          return{
-            swiperOption:{
-              pagination:'.swiper-pagination',
-              loop:true
-            },
-            swiperList:[{
-              id:'0001',
-              imgUrl:'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20195/5eb82a8ca5b0cebb2c3ae1a53a4d8f81.jpg_750x200_645bfba1.jpg'
-            },{
-              id:'0002',
-              imgUrl:'http://img1.qunarzz.com/piao/fusion/1609/4d/3dc09bcc290f0c02.jpg_750x200_09c97d67.jpg'
-            }]
-          }
+  export default {
+    name: "HomeSwiper",
+    data(){
+      return{
+        swiperOption:{
+          pagination:'.swiper-pagination',
+          loop:true
+        },
+        swiperList:[{
+          id:'0001',
+          imgUrl:'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20195/5eb82a8ca5b0cebb2c3ae1a53a4d8f81.jpg_750x200_645bfba1.jpg'
+        },{
+          id:'0002',
+          imgUrl:'http://img1.qunarzz.com/piao/fusion/1609/4d/3dc09bcc290f0c02.jpg_750x200_09c97d67.jpg'
+        }]
       }
     }
+  }
 </script>
 
 <style lang="stylus" scoped>
@@ -42,9 +42,10 @@
     overflow hidden
     width 100%
     height 0
-    padding-bottom 31.25%
+    padding-bottom 26%
     background #eee
   .swiper-img
     width 100%
+    height 100%
 
 </style>
