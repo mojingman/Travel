7508bacb80527177d69da601ba5f95f167f6d2df
mojingman <1393222082@qq.com>
2019-06-19 15:33:34 +0800

    add css to city

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="list">
+      <div class="item">A</div>
+      <div class="item">A</div>
+      <div class="item">A</div>
+      <div class="item">A</div>
+      <div class="item">A</div>
+      <div class="item">A</div>
+      <div class="item">A</div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "CityAlphabet"
+    }
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .list{
+    display flex
+    flex-direction column
+    justify-content center
+    position: absolute;
+    top 0.5rem
+    right 0
+    bottom 0
+    width .4rem
+  }
+  .item{
+    line-height .4rem
+    text-align center
+    color $bgColor
+  }
+
+</style>
