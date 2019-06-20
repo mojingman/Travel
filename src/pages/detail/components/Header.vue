8e5c71468849bec96fdc28e5c964eaf76bb69017
mojingman <1393222082@qq.com>
2019-06-20 16:31:49 +0800

    fix problem

--- src/pages/detail/components/Header.vue
+++ src/pages/detail/components/Header.vue
@@ -52,6 +52,9 @@
       },
       activated(){
        window.addEventListener('scroll',this.handleScroll)
+      },
+      deactivated(){
+          window.removeEventListener('scroll',this.handleScroll)
       }
     }
 </script>
